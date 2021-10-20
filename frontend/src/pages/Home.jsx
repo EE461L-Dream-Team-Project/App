254bbb9fc6f40116f35725c5356e27617ed80a54
Daniel Sosa <danielsosa521@utexas.edu>
2021-10-20 16:21:21 -0500

    Added Home Page Elements

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Button } from "antd";
+
 export default function Home() {
   return (
     <div>
-      <Button>hello</Button>
+      <h1>Projecture</h1>
+      <div className="buttons">
+      <Button>Login</Button>
+      <Button>Register</Button>
+      </div>
+      <h2>Project Description</h2>
+      <h4>Team Member Information</h4>
     </div>
   );
 }
