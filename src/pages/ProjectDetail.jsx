bdc35a0ad27aa428470366b457cc91d9955be922
Ti-tanium <1158976264@qq.com>
2021-11-07 08:58:32 -0600

    Integrate Project API (#10)
    * Integrate Project API
    
    * add id

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { get } from "../request";
 
 export default function ProjectDetail(props) {
   const { projectId } = useParams();
-  return <div>{projectId}</div>;
+  const [project, setProject] = useState({});
+  const fetchProject = async () => {
+    const project = await get(`/project/${projectId}`);
+    setProject(project.data);
+  };
+
+  useEffect(() => {
+    fetchProject();
+  }, []);
+  return <div>{JSON.stringify(project)}</div>;
 }
