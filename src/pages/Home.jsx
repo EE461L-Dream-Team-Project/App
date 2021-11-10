7781a061e49689a42b9efd855dae7146de2c64a8
Prathik Srinivasan <Prathik.Srinivasan@utexas.edu>
2021-11-09 22:36:19 -0600

    carousel+buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,63 @@
 import React from "react";
 import MemberItem from "../components/MemberItem";
-import {List} from 'antd';
+import {List, Button, Space, Card, Carousel} from 'antd';
+import {Link} from 'react-router-dom';
 import "./css/Home.css"
 
 export default function Home() {
   const mediaPrefix = "/media/members/"
+  const {Meta} = Card;
   return (
     <div>
       {" "}
-      <img style={{ with: 20, height: 50, float: "left" }} src="logo.png" />
+      <img style={{height: 150, float: "center" }} src="logo.png" />
+      <br />
+      <div className="buttons-list" id="buttons">
+        <br />
+        <Space direction = "vertical" align = "center" >
+        <Link to="/Login">
+          <Button type="primary" size={'large'} block={true}>Login</Button>
+        </Link>
+        <br />
+        <Link to="/Register">
+          <Button type="primary" size={'large'} block={true}>Register</Button>
+        </Link>
+        </Space>
+      </div>
+      <div className="member-carousel" id="members-carousel">
+        <Carousel dotPosition="top" autoplay>
+          <div>
+          <Card style={{height:500, width: 200 }} cover={<img alt="Shawn Li" src={mediaPrefix+"ShawnLi.jpg"} style={{height:200, width:200}}/>}>
+            <Meta title="Shawn Li" description="description" />
+          </Card>
+          </div>
+          <div>
+          <Card style={{height:500, width: 300 }} cover={<img alt="Sidharth Nair" src={mediaPrefix+"SidharthNair.jpg"} style={{height:200, width:200}}/>}>
+            <Meta title="Sidharth Nair" description="description" />
+          </Card>
+          </div>
+          <div>
+          <Card style={{height:500, width: 300 }} cover={<img alt="Prathik Srinivasan" src={mediaPrefix+"PrathikSrinivasan.jpg"} />}>
+            <Meta title="Prathik Srinivasan" description="description" />
+          </Card>
+          </div>
+          <div>
+          <Card style={{height:500, width: 300 }} cover={<img alt="Nick Taylor" src={mediaPrefix+"NickTaylor.jpg"} />}>
+            <Meta title="Nick Taylor" description="description" />
+          </Card>
+          </div>
+          <div>
+          <Card style={{height:500, width: 300 }} cover={<img alt="Joseph Lawler" src={mediaPrefix+"JosephLawler.jpg"} />}>
+            <Meta title="Joseph Lawler" description="description" />
+          </Card>
+          </div>
+          <div>
+          <Card style={{height:500, width: 300 }} cover={<img alt="Daniel Sosa" src={mediaPrefix+"DanielSosa.jpg"} />}>
+            <Meta title="Daniel Sosa" description="description" />
+          </Card>
+          </div>
+        </Carousel>
+      </div>
       <div className="member-list" id="members">
         <ul id="name-list">
           <li><MemberItem name="Shawn Li"
